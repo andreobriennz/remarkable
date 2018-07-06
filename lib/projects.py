7bc414161863eb77e9c1c8b2db3fd32b0c647d13
andreobriennz <andre@communica.co.nz>
2018-07-06 17:41:32 +1200

    Add ability to generate index.html page with variables etc

--- lib/projects.py
+++ lib/projects.py
@@ -1,33 +1,26 @@
 import imp
 tasks = imp.load_source('tasks', 'settings/projects.py')
-
 load = imp.load_source('loader', 'lib/loader.py')
 saver = imp.load_source('saver', 'lib/saver.py')
-# router = imp.load_source('router', 'routes.py')
-
-# path = '/blog/welcome'
-
-# route = router.routes['posts'][path]
-
-# post = load.html(route['file'])
-
-# html = post
-
-# saver.save(html, 'public/blog.html')
-
-# print(post['html'])
 
 def index():
     task = tasks.tasks[0]
 
+    name = tasks.tasks[0]['name']
+    style = tasks.tasks[0]['theme'] != '' and tasks.tasks[0]['theme'] or tasks.tasks[0]['style']
     root = task['root_file']
-
-    # loop:
-    html = ''
-
+    
     markdowns = task['sections']
+    main = ''
     for markdown in markdowns:
-        print( 'src/' + root + markdown )
-        html += load.html('src/' + root + markdown)['html'] + '\n\n'
+        markup = load.html( 'src/' + root + markdown )
+        main += "<article id='{}'>\n{}\n</article>\n\n".format( markdown, markup )
 
-    saver.save(html, 'public/' + root + 'index.html')
+    page = load.raw( 'lib/theming/index.html' ).format(
+        title = name.title(),
+        main = main,
+        style = name,
+        script = name,
+    )
+
+    saver.save(page, 'public/' + root + 'index.html')
