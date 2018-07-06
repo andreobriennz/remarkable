53d208d0e58a785a7b2d64d93a4d4d95035ea0bf
andreobriennz <andre@communica.co.nz>
2018-07-06 17:43:02 +1200

    Tidy file

--- lib/saver.py
+++ lib/saver.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import markdown
-# import bleach
 
 def save(html, file):
     test_file = open(file, 'wb')
@@ -9,26 +8,3 @@ def save(html, file):
     test_file.write( bytes( html, 'utf-8' ) )
 
     test_file.close()
-
-
-# # read/write file
-# test_file = open('test-files/file1.md', 'wb')
-
-# print( test_file.mode ) # 'wb'
-# print( test_file.name ) 
-
-# test_file.write( bytes("# Added to the file").encode("utf-8") )
-
-# test_file.close()
-
-# print( test_file.mode ) # 'wb'
-
-
-# test_file = open('test-files/file1.md', 'r+') # read/write
-# text = test_file.read()
-# print(text)
-
-# # os.remove('test-files/file1.md') # delete file
-
-# html = markdown.markdown(text)
-# print(html)
