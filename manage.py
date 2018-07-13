5c899224ac5a2dca5fd471b1de21a5901bf6a85d
andreobriennz <andre@communica.co.nz>
2018-07-13 12:56:17 +1200

    Create CLI

--- manage.py
+++ manage.py
@@ -1,10 +1,22 @@
 import imp
-projects = imp.load_source('projects', 'core/lib/projects.py')
+projects = imp.load_source('projects', 'core/lib/compile.py')
+
+import shutil
+
+version = '0.0.0'
 
 # run with: runp manage.py test
-# def test():
-#     print('test passed!')
+def test( test_string = '' ):
+    if test_string != '':
+        print('Test passed! Message: '+test_string)
+    else:
+        print('Test passed!')
+    
 
-def index():
-    projects.index()
-index()
+def run( project_name ):
+    projects.index( project_name)
+    print('Compiled!')
+
+def create( project_name ):
+    shutil.copytree('core/default_project', 'projects/'+project_name)
+    print('Created in: projects/'+project_name+'!')
