21c229026b7d1ca8fc5fe95a4834078f60a07428
andreobriennz <andre@communica.co.nz>
2018-07-13 13:18:07 +1200

    Automatically edit .json

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 import imp
-projects = imp.load_source('projects', 'core/lib/compile.py')
-
 import shutil
+import json
+projects = imp.load_source('projects', 'core/lib/compile.py')
 
 version = '0.0.0'
 
@@ -19,4 +19,20 @@ def run( project_name ):
 
 def create( project_name ):
     shutil.copytree('core/default_project', 'projects/'+project_name)
+
+    new_project_data = {
+        "name": project_name,
+        "theme": "",
+        "title": "",
+        "subtitle": "",
+        "sections": [],
+    }
+
+    with open('projects/projects.json', 'r+') as f:
+        data = json.load(f)
+        data['projects'][project_name] = new_project_data
+        f.seek(0)        # <--- should reset file position to the beginning.
+        json.dump(data, f, indent = 4)
+        f.truncate()     # remove remaining part
+
     print('Created in: projects/'+project_name+'!')
