90d2f719d06e04b23af4e0fc5e297cfc06f26d1c
andreobriennz <andre@communica.co.nz>
2018-07-17 13:05:17 +1200

    Improve CLI

--- manage.py
+++ manage.py
@@ -1,24 +1,26 @@
 import imp
 import shutil
 import json
+import argparse
+
 projects = imp.load_source('projects', 'core/lib/compile.py')
 
 version = '0.0.0'
 
 # run with: runp manage.py test
-def test( test_string = '' ):
+def test(test_string = ''):
     if test_string != '':
         print('Test passed! Message: '+test_string)
     else:
         print('Test passed!')
     
 
-def run( project_name ):
+def run(project_name):
     projects.index( project_name)
     print('Compiled!')
 
 
-def create( project_name ):
+def create(project_name):
     shutil.copytree('core/default_project', 'projects/'+project_name)
 
     new_project_data = {
@@ -41,3 +43,23 @@ def create( project_name ):
 
 def start_gui():
     gui_create_project = imp.load_source('gui_create_project', 'core/GUI/create_project.py')
+
+
+# argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('task')
+
+args = parser.parse_args()
+
+if args.task:
+    if args.task == 'version':
+        print('Version: '+version)
+
+    elif args.task == 'gui' or args.task == 'open' or args.task == 'start':
+        start_gui()
+    
+    elif 'compile=' in args.task:
+        run(args.task.split('compile=')[1])
+    
+    elif 'create=' in args.task:
+        create(args.task.split('create=')[1])
