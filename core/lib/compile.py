a581a9510b2817b2efb9894dcdcbc86bb7ae42b6
andreobriennz <andre@communica.co.nz>
2018-07-17 12:02:48 +1200

    Allow wildcard(*) in project.json

--- core/lib/compile.py
+++ core/lib/compile.py
@@ -42,12 +42,30 @@ def index( project_name ):
     
     markdowns = project_details['sections']
     main = ''
-    for markdown_path in markdowns:
-        markup = load.html( root+'content/'+markdown_path+'.md' )
-        markdown_path = markdown_path.replace('/', '__')
-        main += "<article id='{}'>\n{}\n</article>\n\n".format( markdown_path, markup )
-        
-        saver.save( markup, 'public/'+name+'/content/'+markdown_path+'.html' )
+
+    def get_html(markdown_paths):
+        main = ''
+        for markdown_path in markdown_paths:
+            if '*' in markdown_path:
+                markdown_path = markdown_path.split('*')[0]
+                files = os.listdir(root+'content/'+markdown_path)
+                files = sorted(files)
+
+                markdown_paths = []
+                for file in files:
+                    markdown_paths.append(markdown_path+file[:-3])
+                main += get_html(markdown_paths)
+                    
+            else:
+                markup = load.html( root+'content/'+markdown_path+'.md' )
+                markdown_path = markdown_path.replace('/', '__')
+                main += "<article id='{}'>\n{}\n</article>\n\n".format( markdown_path, markup )
+                
+                saver.save(markup, 'public/'+name+'/content/'+markdown_path+'.html')
+
+        return main
+    
+    main += get_html(markdowns)
 
     style = load_section(
         root+'assets/custom.css'
