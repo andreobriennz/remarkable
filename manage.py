d840c8447c277a3d61dc53ac15790711aba18ff6
andreobriennz <andre@communica.co.nz>
2018-07-20 12:39:18 +1200

    Tidy and simplify manage.py

--- manage.py
+++ manage.py
@@ -1,45 +1,30 @@
-from core.lib import create
-from core.lib import compile_project
-from core.GUI import index as gui
+from core.lib.crud import create
+from core.lib.crud import _compile
+from core.views import index as gui
 
 import argparse
+import sys
 
 
-version = '0.0.0'
+version = '1.0.0-alpha.2'
 
 
-# run with: runp manage.py test
-def test(test_string=''):
-    if test_string != '':
-        print('Test passed! Message: '+test_string)
-    else:
-        print('Test passed!')
-    
-
-def run(project_name):
-    compile_project.index(project_name)
-    print('Compiled!')
-
-
-def start_gui():
-    gui.index()
-
-
-# argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('task')
 
 args = parser.parse_args()
 
-if args.task:
-    if args.task == 'version':
-        print('Version: '+version)
+if args.task == 'version' or args.task == 'v':
+    print('Remarkable: v'+version)
+    print('Python: v'+sys.version)
 
-    elif args.task == 'gui' or args.task == 'open' or args.task == 'start':
-        start_gui()
-    
-    elif 'compile=' in args.task:
-        run(args.task.split('compile=')[1])
-    
-    elif 'create=' in args.task:
-        create.create(args.task.split('create=')[1])
+elif args.task == 'start' or args.task == 'open':
+    gui.index()
+
+elif 'compile=' in args.task:
+    project = args.task.split('compile=')[1]
+    _compile.compile(project)
+
+elif 'create=' in args.task:
+    project = args.task.split('create=')[1]
+    create.project(project)
