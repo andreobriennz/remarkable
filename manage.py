ac18c02c82f0cfe6691cbfe14b782d8eb7d8a87b
andreobriennz <andre@communica.co.nz>
2018-07-20 13:45:30 +1200

    tidy code

--- manage.py
+++ manage.py
@@ -17,14 +17,11 @@ args = parser.parse_args()
 if args.task == 'version' or args.task == 'v':
     print('Remarkable: v'+version)
     print('Python: v'+sys.version)
-
 elif args.task == 'start' or args.task == 'open':
     gui.index()
-
 elif 'compile=' in args.task:
     project = args.task.split('compile=')[1]
     _compile.compile(project)
-
 elif 'create=' in args.task:
     project = args.task.split('create=')[1]
     create.project(project)
